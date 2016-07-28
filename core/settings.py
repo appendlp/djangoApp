af03dd9797c9a02e3c14a190f752c6910037ebf3
lupeng <mayday0608@hotmail.com>
2016-07-28 17:18:51 +0000

    fix setting

--- core/settings.py
+++ core/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -95,7 +95,7 @@ DATABASES = {
                 'PORT': 5432, 
             }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
