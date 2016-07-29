aa006c5ac93894e46f0e9907cc3ad05b724586ec
lupeng <mayday0608@hotmail.com>
2016-07-29 13:52:15 +0000

    fix sample folder

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^sample/', include('sample.urls')),
+    url(r'^sample/', include('apps.sample.urls')),
     url(r'^admin/', admin.site.urls),
 ]
